b9d85d00d84eb4f8776637f965ec416b5b25ab7e
Raziye <rosemiller.developer@gmail.com>
2022-04-05 00:55:02 +0430

    refactor class component to functional

--- src/Components/IpInfo.jsx
+++ src/Components/IpInfo.jsx
@@ -3,39 +3,34 @@ import InfoCard from "./InfoCard";
 import getInfo from "../Services/getInfo";
 import Loader from "react-loader-spinner";
 
-export default class IpInfo extends Component {
-  constructor() {
-    super();
-    this.state = {
-      info: null,
-      isLoading: true,
-    };
-  }
+export default function IpInfo() {
+  const [appState, setAppState] = React.useState({
+    info: null,
+    isLoading: true,
+  });
 
-  componentDidMount() {
+  React.useEffect(() => {
     getInfo().then((data) => {
-      this.setState({
+      setAppState({
         info: data,
         isLoading: false,
       });
     });
-  }
+  }, []);
 
-  render() {
-    return (
-      <>
-        {this.state.isLoading ? (
-                <Loader
-                type="Puff"
-                color="#00BFFF"
-                height={100}
-                width={100}
-                timeout={3000} //3 secs
-              />
-        ) : (
-          <InfoCard info={this.state.info} />
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      {appState.isLoading ? (
+        <Loader
+          type="Puff"
+          color="#00BFFF"
+          height={100}
+          width={100}
+          timeout={3000} //3 secs
+        />
+      ) : (
+        <InfoCard info={appState.info} />
+      )}
+    </>
+  );
 }
